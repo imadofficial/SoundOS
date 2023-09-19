2096665bf4ae248bb5fbb920b1e4eb81b94cc6b9
Imad <59332263+imadofficial@users.noreply.github.com>
2023-09-19 15:56:42 +0200

    Add files via upload

---
+++ PiMusic/Statusbar.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiMusic
+{
+    /// <summary>
+    /// Interaction logic for Statusbar.xaml
+    /// </summary>
+    public partial class Statusbar : Page
+    {
+        public Statusbar()
+        {
+            InitializeComponent();
+            BatteryPerc();
+        }
+
+        private void BatteryPerc()
+        {
+            System.Management.ObjectQuery query = new ObjectQuery("Select * FROM Win32_Battery");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+            ManagementObjectCollection collection = searcher.Get();
+
+            foreach (ManagementObject mo in collection)
+            {
+                foreach (PropertyData property in mo.Properties)
+                {
+                    Console.WriteLine("Property {0}: Value is {1}", property.Name, property.Value);
+                    //BatteryPercentage.Text = property.Value.ToString();
+                }
+            }
+
+        }
+    }
+}
