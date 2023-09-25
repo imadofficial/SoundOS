9bccaf42ebf6d82becabbe0dec20deea278a864b
Imad <59332263+imadofficial@users.noreply.github.com>
2023-09-25 10:51:13 +0200

    Added placeholder for Music & modified navbar

--- PiMusic/obj/Debug/net6.0-windows/Splash.g.i.cs
+++ PiMusic/obj/Debug/net6.0-windows/Splash.g.i.cs
@@ -53,7 +53,7 @@ namespace PiMusic {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PiMusic;component/splash.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PiMusic;V1.0.0.0;component/splash.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Splash.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
